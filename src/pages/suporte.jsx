d5ad0ec4276f4f14d24ede501ebf4c30a14f4ca2
gigomesamaral1 <giovanna.amaral@tecnico.ulisboa.pt>
2023-05-17 16:28:50 -0300

    pagina de suporte

--- src/pages/suporte.jsx
+++ src/pages/suporte.jsx
@@ -1,43 +1,31 @@
-<<<<<<< HEAD
-import Menu from "../components/Menu/Menu";
-import Cartao from "../components/Cartao/Cartao";
-import { Tudo } from "../components/Cartao/style";
-
-
-const suporte = () => (
-    <Tudo>
-    <Menu />
-    <h1 class="titulos">Suporte</h1>
-    <h2 class="titulos">Horário de funcionamento</h2>
-    <Cartao 
-        simb={"Telefone"}
-    />
-    <Cartao 
-        simb={"Email"}
-    />
-    <Cartao 
-        simb={"Chat"}
-    />
-    </Tudo>
-);
-export default suporte;
-=======
 import React from "react";
 import Menu from "../components/Menu/Menu";
 import Cartao from "../components/Cartao/Cartao";
 import { Tudo } from "../components/Cartao/style";
 import "../pages/style.css";
+import logo from './minha_foto.jpg';
 
-const Suporte = () => (
-  <Tudo>
+
+function Suporte() {
+  return (
+  <div className="Suporte">
     <Menu />
-    <h1 className="titulos">Suporte</h1>
-    <h2 className="titulos">Horário de funcionamento</h2>
-    <Cartao simb="Telefone" />
-    <Cartao simb="Email" />
-    <Cartao simb="Chat" />
-  </Tudo>
+    <header className="Suporte-logo">
+      <img src={logo} className="Suporte-logo" alt="logo" />
+    </header>
+    <body className='Suporte-area'>
+      <h1>Suporte TechMais</h1>
+      <p>Email</p>  
+      <h2>suporte@techmais.com</h2>     
+      <p>Telefone</p>
+      <h2>(21)9999-8888 /  (11) 98769-5555</h2> 
+      <p>Horário de funcionamento: 24h</p>              
+    </body>
+    <footer className='footer'>
+      <p>Desenvolvido por @Techmais. Todos os direitos reservados. </p>
+    </footer>
+  </div>
 );
+}
 
 export default Suporte;
->>>>>>> c8d2eb2652eff0d0203420e4e769ada2d35c1fb4
